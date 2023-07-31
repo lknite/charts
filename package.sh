a84e0ae75d476c8e8bbba3c3101d5f255d471776
leland knight <lknite@aarr.xyz>
2023-07-31 14:13:09 -0400

    initial index generation

--- package.sh
+++ package.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+#
 helm package charts/argocd-register-controller --dependency-update
 helm package charts/argocd-pinniped-controller --dependency-update
+
+#
+helm repo index --url https://lknite.github.io .
