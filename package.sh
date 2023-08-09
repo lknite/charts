68a0754f995502625e270bef7a859339fd6857a8
leland knight <lknite@aarr.xyz>
2023-08-09 15:42:33 -0400

    update charts

--- package.sh
+++ package.sh
@@ -3,10 +3,12 @@
 #
 cp -r ../daytwo/daytwo-argocd-register-controller/chart/* charts/argocd-register-controller
 cp -r ../daytwo/daytwo-argocd-pinniped-controller/chart/* charts/argocd-pinniped-controller
+cp -r ../daytwo/argocd-sync-labels-controller/chart/* charts/argocd-sync-labels-controller
 
 #
 helm package charts/argocd-register-controller --dependency-update
 helm package charts/argocd-pinniped-controller --dependency-update
+helm package charts/argocd-sync-labels-controller --dependency-update
 
 #
 helm repo index --url https://lknite.github.io/charts .
