8680fac68082beebdb34a428adbaabe0655e4395
leland knight <lknite@aarr.xyz>
2023-08-09 15:44:09 -0400

    update charts

--- package.sh
+++ package.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 #
+mkdir -p charts/argocd-register-controller
 cp -r ../daytwo/daytwo-argocd-register-controller/chart/* charts/argocd-register-controller
+mkdir -p charts/argocd-pinniped-controller
 cp -r ../daytwo/daytwo-argocd-pinniped-controller/chart/* charts/argocd-pinniped-controller
+mkdir -p charts/argocd-sync-labels-controller
 cp -r ../daytwo/argocd-sync-labels-controller/chart/* charts/argocd-sync-labels-controller
 
 #
