0fcee9da3d050225549b9910020da57424d15749
leland knight <lknite@aarr.xyz>
2023-08-10 11:41:24 -0400

    update

--- package.sh
+++ package.sh
@@ -4,7 +4,7 @@
 mkdir -p charts/argocd-register-controller
 cp -r ../daytwo/argocd-register-controller/chart/* charts/argocd-register-controller
 mkdir -p charts/argocd-pinniped-controller
-cp -r ../daytwo/daytwo-argocd-pinniped-controller/chart/* charts/argocd-pinniped-controller
+cp -r ../daytwo/argocd-pinniped-controller/chart/* charts/argocd-pinniped-controller
 mkdir -p charts/argocd-labels-controller
 cp -r ../daytwo/argocd-labels-controller/chart/* charts/argocd-labels-controller
 
